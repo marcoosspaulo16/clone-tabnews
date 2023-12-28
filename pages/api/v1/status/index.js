079ca4962b017ad162c1bd1dcab8fa921c679c88
marcoosspaulo16 <62853847+marcoosspaulo16@users.noreply.github.com>
2023-12-28 14:14:25 +0000

    config files  and docker

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,12 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+process.on("uncaughtException", function (err) {
+  console.log(err);
+});
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   response.status(200).json({ chave: "Sao acima da media" });
 }
 
