c345ef4edfb165e03ec9beca25719d1cc0e7a1c3
Marcos Paulo <62853847+marcoosspaulo16@users.noreply.github.com>
2024-07-16 23:50:24 +0000

    adds /api/v1/migrations endpoin

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,12 +1,41 @@
 import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
+import database from "infra/database.js";
 
 export default async function migrations(request, response) {
-  const migrations = await migrationRunner({
-    databaseUrl: process.env.DATABASE_URL,
+  const dbClient = await database.getNewClient();
+
+  const defaultMigrationOptions = {
+    dbClient: dbClient,
     dryRun: true,
     dir: join("infra", "migrations"),
     direction: "up",
-  });
-  response.status(200).json(migrations);
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+
+  console.log(request.method);
+
+  if (request.method === "GET") {
+    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+    await dbClient.end();
+    return response.status(200).json(pendingMigrations);
+  }
+
+  if (request.method === "POST") {
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dryRun: false,
+    });
+
+    await dbClient.end();
+
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations);
+    }
+
+    return response.status(200).json(migratedMigrations);
+  }
+
+  return response.status(405).end();
 }
--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,8 +1,21 @@
+import database from "infra/database.js";
+
+beforeAll(cleanDataBase);
+
+async function cleanDataBase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
+
 test("GET to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
   expect(response.status).toBe(200);
 
+  // console.log(responseBody);
+  // console.log("Ambiente: ", process.env.NODE_ENV);
+  // console.log("POSTGRES_DB: ", process.env.POSTGRES_DB);
+
   const responseBody = await response.json();
 
   expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
 });
